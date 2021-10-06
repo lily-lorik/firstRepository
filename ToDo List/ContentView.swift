5ff3cb25967bc1b5cc9ed1830078932c452df67e
Lagoda Liliia <liliana.lag12@gmail.com>
2021-10-06 14:33:09 -0700

    ToDo List project

---
+++ ToDo List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo List
+//
+//  Created by Лагода Лілія on 06.10.2021.
+//  Copyright © 2021 Лагода Лілія. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
