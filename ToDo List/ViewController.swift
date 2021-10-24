68ce2809045e2dba697f070d6cad5a4ce746fbb7
Lagoda Liliia <liliana.lag12@gmail.com>
2021-10-24 14:49:57 -0700

    homework lessons 7-8. Autolayout + OOP

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Лагода Лілія on 24.10.2021.
+//  Copyright © 2021 Лагода Лілія. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
