91a85a0ff9c3201d8352cc80e5efb43bb601b869
Lagoda Liliia <liliana.lag12@gmail.com>
2021-10-24 14:50:43 -0700

    homework lessons 7-8. Autolayout + OOP

--- ToDo List/BaseViewController.swift
+++ ToDo List/BaseViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class BaseViewController: UIViewController {
+    
+    var backgroundColor: UIColor = .orange
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = backgroundColor
+        view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
     
--- ToDo List/ViewController.swift
+++ ToDo List/ViewController.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmEnterButton: UIButton!
+    
+    var resultLabel: UILabel!
+    
+    let rightEmail: String = "test@gmail.com"
+    let rightPassword: String = "1234qwer5"
+    
+    @IBAction func loginAction(_ sender: Any){
+        print("loginAction")
+        if let email = emailField.text,
+        let password = passwordField.text{
+
+          if email.contains("@") {
+            if password.count >= 8 {
+              if email == rightEmail {
+                if password == rightPassword {
+                  resultLabel.text = "Success"
+                } else{
+                  resultLabel.text = "Autentification failed. Wrong password"
+                }
+              } else{
+                  resultLabel.text = "Autentification failed. Wrong email"
+              }
+            } else{
+                  resultLabel.text = "Password must have at least  8 symbols"
+              }
+            }else{
+                  resultLabel.text = "The Email adress must have @"
+              }
+        }else {
+                resultLabel.text = "Fill in fields"
+              }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        resultLabel = UILabel.init()
+        resultLabel.text = "Result label"
+        resultLabel.frame = CGRect.init(x:50, y:50, width: 50, height: 20)
+        resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.init(item: resultLabel!,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: confirmEnterButton,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: -20).isActive = true
+        
+        NSLayoutConstraint.init(item: resultLabel!,
+                            attribute: .centerX,
+                            relatedBy: .equal,
+                            toItem: view,
+                            attribute: .centerX,
+                            multiplier: 1,
+                            constant: 0).isActive = true
+        view.addSubview(resultLabel)
         // Do any additional setup after loading the view.
     }
     
